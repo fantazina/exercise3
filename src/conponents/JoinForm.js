2e0f107481df91647cc2c0c793f52f9efe670cd8
fantazina <137284904+fantazina@users.noreply.github.com>
2024-02-19 14:41:38 +0900

    로그인, 회원가입 수정

--- src/conponents/JoinForm.js
+++ src/conponents/JoinForm.js
@@ -9,7 +9,7 @@ const JoinForm = ({onFormPage}) => {
         pwd : '',
         pwd_re : ''
     })
-
+    const[joinError, setJoinError] = useState(true)
     const[nameError, setNameError] = useState('')
     const[idError, setIdError] = useState('')
     const[pwdError, setPwdError] = useState('')
@@ -31,47 +31,71 @@ const JoinForm = ({onFormPage}) => {
         axios.get(`http://localhost:8080/user/isExistId/${id}`)
              .then(res => {
                 setIdError(res.data === 'non_exist' ? '사용 가능' : '사용 불가능')
+                setJoinError(res.data === 'non_exist' ? false : true)
             })
              .catch(error => console.log(error))
     }
 
-    const onWriteSubmit = (e) => {
+    const onJoinSubmit = (e) => {
 
         var sw = 1
 
+        
         if(!userDTO.name) {
             setNameError('이름을 입력하세요.')
             sw = 0 
+            
         } else {
             setNameError('')
         }
+        
         if(!userDTO.id) {
             setIdError('아이디를 입력하세요.')
             sw = 0
+            
         } else {
             setIdError('')
         }
+
+        if(joinError) {
+            setIdError('중복된 아이디입니다.')
+            sw = 0
+
+        } else {
+            setIdError('')
+        }
+
         if(!userDTO.pwd) {
             setPwdError('비밀번호를 입력하세요.')
             sw = 0
+
         } else {
             setPwdError('')
         }
+
         if(!userDTO.pwd_re) {
             setPwd_reError('비밀번호를 다시 입력하세요.')
             sw = 0
+
         } else {
             setPwd_reError('')
         }
 
-        if(sw === 1){
-            axios.post(`http://localhost:8080/user/write`, userDTO )
+        if(sw === 1) {
+            axios.post(`http://localhost:8080/user/write`, userDTO)
                  .then(() => {
 
                      alert('회원가입을 축하합니다.')
                      onFormPage(1)
-                    }).catch(error => alert('회원가입 실패'))
-          }
+
+                    }).catch(error => {
+                        alert('회원가입 실패');
+                        onFormPage(0);
+                    });
+                } else {
+                    alert('아이디를 확인하세요.')
+                    onFormPage(0)
+                }
     }
 
     const onReset = () => {
@@ -111,7 +135,7 @@ const JoinForm = ({onFormPage}) => {
             <div className={styles.check}>{pwd_reError}</div>
 
             <div className={ styles.btn_div }>
-                <button onClick={ onWriteSubmit }>회원가입</button>
+                <button onClick={ onJoinSubmit }>회원가입</button>
                 <button className={ styles.reset } onClick={ onReset }>취소</button>
             </div>
         </div>
